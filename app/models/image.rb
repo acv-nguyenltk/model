9125e8c44719e8837e7bde3fe2eee76c99d19802
Nguyen <nguyenltk@atmarkcafe.org>
2018-08-08 09:15:47 +0700

    Create DB and seed data

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
-  belong_to :article
+  belongs_to :article
   validates :order, presence:true, uniqueness: true
   validates :like_no, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/text.rb
+++ app/models/text.rb
@@ -1,6 +1,5 @@
 class Text < ApplicationRecord
-  belong_to :article
+  belongs_to :article
   validates :order, presence:true, uniqueness: true
   validates :like_no, presence: true, numericality: { greater_than_or_equal_to: 0 }
-
 end
